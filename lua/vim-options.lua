07a08a64384e3768d0f3da0a9e5f945adc628444
Bernardo da Eira Duarte <bernardoeiraduarte@gmail.com>
2024-01-07 11:34:40 -0300

    Windows WSL clipboard setup for Neovim

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,8 +2,23 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
+vim.cmd("set smartindent")
 vim.g.mapleader = " "
 
 vim.opt.guicursor = ""
 vim.opt.nu = true
 vim.opt.relativenumber = true
+
+-- https://neovim.io/doc/user/provider.html#clipboard-wsl
+vim.g.clipboard = {
+    name = "WslClipboard",
+    copy = {
+        ["+"] = "clip.exe",
+        ["*"] = "clip.exe",
+    },
+    paste = {
+        ["+"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).toString().replace("`r", ""))',
+        ["*"] = 'powershell.exe -c [Console]::Out.Write($(Get-Clipboard -Raw).toString().replace("`r", ""))',
+    },
+    cache_enabled = true,
+}
